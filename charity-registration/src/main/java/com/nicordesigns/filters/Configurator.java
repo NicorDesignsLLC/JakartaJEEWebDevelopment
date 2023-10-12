3cd11d9825c9b5fba6b118ba1de129ec5040f6b1
NicorDesigns <nicocpk@gmail.com>
2023-10-12 06:35:49 -0400

    Beginning of Authentication Filter

--- charity-registration/src/main/java/com/nicordesigns/filters/Configurator.java
+++ charity-registration/src/main/java/com/nicordesigns/filters/Configurator.java
@@ -10,14 +10,20 @@ import javax.servlet.annotation.WebListener;
 public class Configurator implements ServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
+
 		ServletContext context = event.getServletContext();
 
-		FilterRegistration.Dynamic registration = context.addFilter("requestLogFilter", new RequestLogFilter());
-		registration.addMappingForUrlPatterns(null, false, "/*");
+//		FilterRegistration.Dynamic registration = context.addFilter("requestLogFilter", new RequestLogFilter());
+//		registration.addMappingForUrlPatterns(null, false, "/*");
+//
+//		registration = context.addFilter("compressionFilter", new CompressionFilter());
+//		registration.setAsyncSupported(true);
+//		registration.addMappingForUrlPatterns(null, false, "/*");
 
-		registration = context.addFilter("compressionFilter", new CompressionFilter());
+		FilterRegistration.Dynamic registration = context.addFilter("authenticationFilter", new AuthenticationFilter());
 		registration.setAsyncSupported(true);
-		registration.addMappingForUrlPatterns(null, false, "/*");
+		registration.addMappingForUrlPatterns(null, false, "/registrations", "/sessions");
+
 	}
 
 	@Override
