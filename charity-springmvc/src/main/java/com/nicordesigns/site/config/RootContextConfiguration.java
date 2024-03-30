6297924cb20c0870cbe08edde6449ab512b1df63
NicorDesignsLLC <nicordesigns@gmail.com>
2024-03-30 17:33:53 -0400

    Updating WebSocket Chat to use the Service Layer

--- charity-springmvc/src/main/java/com/nicordesigns/site/config/RootContextConfiguration.java
+++ charity-springmvc/src/main/java/com/nicordesigns/site/config/RootContextConfiguration.java
@@ -3,12 +3,22 @@ package com.nicordesigns.site.config;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.util.concurrent.Executor;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.stereotype.Controller;
 
 @Configuration
@@ -16,10 +26,12 @@ import org.springframework.stereotype.Controller;
 @EnableScheduling
 @ComponentScan(basePackages = "com.nicordesigns.site", 
 excludeFilters = @ComponentScan.Filter(Controller.class)	
-               //useDefaultFilters = false, 
-               //includeFilters = @ComponentScan.Filter(org.springframework.stereotype.Controller.class)
 )
-public class RootContextConfiguration {
+public class RootContextConfiguration implements AsyncConfigurer, SchedulingConfigurer {
+	
+	private static final Logger log = LogManager.getLogger();
+	private static final Logger schedulingLogger =
+	            LogManager.getLogger(log.getName() + ".[scheduling]");
     @Bean
     public ObjectMapper objectMapper()
     {
@@ -37,5 +49,41 @@ public class RootContextConfiguration {
         Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
         marshaller.setPackagesToScan(new String[] { "com.nicordesigns.site" });
         return marshaller;
+    }
+    
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler()
+    {
+        log.info("Setting up thread pool task scheduler with 20 threads.");
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(20);
+        scheduler.setThreadNamePrefix("task-");
+        scheduler.setAwaitTerminationSeconds(60);
+        scheduler.setWaitForTasksToCompleteOnShutdown(true);
+        scheduler.setErrorHandler(t -> schedulingLogger.error(
+                "Unknown error occurred while executing task.", t
+        ));
+        scheduler.setRejectedExecutionHandler(
+                (r, e) -> schedulingLogger.error(
+                        "Execution of task {} was rejected for unknown reasons.", r
+                )
+        );
+        return scheduler;
+    }
+
+    @Override
+    public Executor getAsyncExecutor()
+    {
+        Executor executor = this.taskScheduler();
+        log.info("Configuring asynchronous method executor {}.", executor);
+        return executor;
+    }
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar registrar)
+    {
+        TaskScheduler scheduler = this.taskScheduler();
+        log.info("Configuring scheduled method executor {}.", scheduler);
+        registrar.setTaskScheduler(scheduler);
     }
 }
