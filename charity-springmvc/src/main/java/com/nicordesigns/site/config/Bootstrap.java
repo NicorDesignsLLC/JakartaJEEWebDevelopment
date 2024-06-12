2de6602a9218e552744504fabe102029a118e31f
NicorDesignsLLC <nicordesigns@gmail.com>
2024-06-12 06:24:01 -0400

    The whole of the Moon

--- charity-springmvc/src/main/java/com/nicordesigns/site/config/Bootstrap.java
+++ charity-springmvc/src/main/java/com/nicordesigns/site/config/Bootstrap.java
@@ -16,59 +16,39 @@ import com.nicordesigns.site.filters.AuthenticationFilter;
 import com.nicordesigns.site.filters.LoggingFilter;
 import com.nicordesigns.site.SessionListener;
 
-@SuppressWarnings("unused")
-public class Bootstrap implements WebApplicationInitializer
-{
+public class Bootstrap implements WebApplicationInitializer {
+
     @Override
-    public void onStartup(ServletContext container) throws ServletException
-    {
+    public void onStartup(ServletContext container) throws ServletException {
         container.getServletRegistration("default").addMapping("/resources/**");
 
-        AnnotationConfigWebApplicationContext rootContext =
-                new AnnotationConfigWebApplicationContext();
-        
+        // Root context
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
         rootContext.register(RootContextConfiguration.class);
         container.addListener(new ContextLoaderListener(rootContext));
         container.addListener(SessionListener.class);
 
-        AnnotationConfigWebApplicationContext servletContext =
-                new AnnotationConfigWebApplicationContext();
+        // Dispatcher Servlet for web application
+        AnnotationConfigWebApplicationContext servletContext = new AnnotationConfigWebApplicationContext();
         servletContext.register(ServletContextConfiguration.class);
-        
-        
-        ServletRegistration.Dynamic dispatcher = container.addServlet(
-                "springDispatcher", new DispatcherServlet(servletContext)
-        );
+        ServletRegistration.Dynamic dispatcher = container.addServlet("springDispatcher", new DispatcherServlet(servletContext));
         dispatcher.setLoadOnStartup(1);
-        dispatcher.setMultipartConfig(new MultipartConfigElement(
-                null, 20_971_520L, 41_943_040L, 512_000
-        ));
+        dispatcher.setMultipartConfig(new MultipartConfigElement(null, 20971520L, 41943040L, 512000));
         dispatcher.addMapping("/");
-        
-        //Config context for our SOAP Endpoint
-        AnnotationConfigWebApplicationContext soapContext =
-                new AnnotationConfigWebApplicationContext();
-        soapContext.register(SoapServletContextConfiguration.class);
-        MessageDispatcherServlet soapServlet =
-                new MessageDispatcherServlet(soapContext);
+
+        // Message Dispatcher Servlet for SOAP services
+        MessageDispatcherServlet soapServlet = new MessageDispatcherServlet(servletContext);
         soapServlet.setTransformWsdlLocations(true);
-        dispatcher = container.addServlet("springSoapDispatcher", soapServlet);
-        dispatcher.setLoadOnStartup(3);
-        dispatcher.addMapping("/services/Soap/*");
-        
-        
+        ServletRegistration.Dynamic soapDispatcher = container.addServlet("springSoapDispatcher", soapServlet);
+        soapDispatcher.setLoadOnStartup(2);
+        soapDispatcher.addMapping("/services/Soap/*");
 
-        FilterRegistration.Dynamic registration = container.addFilter(
-                "loggingFilter", new LoggingFilter()
-        );
-        registration.addMappingForUrlPatterns(null, false, "/*");
+        // Logging filter
+        FilterRegistration.Dynamic loggingFilter = container.addFilter("loggingFilter", new LoggingFilter());
+        loggingFilter.addMappingForUrlPatterns(null, false, "/*");
 
-        registration = container.addFilter(
-                "authenticationFilter", new AuthenticationFilter()
-        );
-        registration.addMappingForUrlPatterns(
-                null, false, "/registration", "/registration/*", "/chat", "/chat/*",
-                "/session", "/session/*"
-        );
+        // Authentication filter
+        FilterRegistration.Dynamic authenticationFilter = container.addFilter("authenticationFilter", new AuthenticationFilter());
+        authenticationFilter.addMappingForUrlPatterns(null, false, "/registration", "/registration/*", "/chat", "/chat/*", "/session", "/session/*");
     }
 }
