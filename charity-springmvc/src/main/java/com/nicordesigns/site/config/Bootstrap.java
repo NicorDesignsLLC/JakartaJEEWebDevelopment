cab255a1537d776b58e2cfac3fa33ef416d5b0dc
NicorDesignsLLC <nicordesigns@gmail.com>
2024-08-20 13:52:26 -0400

    Starting to get web rest and soap play together nicely

--- charity-springmvc/src/main/java/com/nicordesigns/site/config/Bootstrap.java
+++ charity-springmvc/src/main/java/com/nicordesigns/site/config/Bootstrap.java
@@ -1,5 +1,11 @@
 package com.nicordesigns.site.config;
 
+import javax.servlet.FilterRegistration;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -9,24 +15,14 @@ import com.nicordesigns.site.filters.AuthenticationFilter;
 import com.nicordesigns.site.filters.LoggingFilter;
 import com.nicordesigns.site.SessionListener;
 
+public class Bootstrap implements WebApplicationInitializer {
 
-import javax.servlet.FilterRegistration;
-import javax.servlet.MultipartConfigElement;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-
-@SuppressWarnings("unused")
-public class Bootstrap implements WebApplicationInitializer
-{
     @Override
-    public void onStartup(ServletContext container) throws ServletException
-    {
+    public void onStartup(ServletContext container) throws ServletException {
         container.getServletRegistration("default").addMapping("/resources/**");
 
-        AnnotationConfigWebApplicationContext rootContext =
-                new AnnotationConfigWebApplicationContext();
-        
+        // Root context
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
         rootContext.register(RootContextConfiguration.class);
         container.addListener(new ContextLoaderListener(rootContext));
         container.addListener(SessionListener.class);
@@ -34,28 +30,22 @@ public class Bootstrap implements WebApplicationInitializer
         AnnotationConfigWebApplicationContext servletContext =
                 new AnnotationConfigWebApplicationContext();
         servletContext.register(ServletContextConfiguration.class);
-        
-        
-        ServletRegistration.Dynamic dispatcher = container.addServlet(
-                "springDispatcher", new DispatcherServlet(servletContext)
-        );
-        dispatcher.setLoadOnStartup(1);
-        dispatcher.setMultipartConfig(new MultipartConfigElement(
-                null, 20_971_520L, 41_943_040L, 512_000
-        ));
-        dispatcher.addMapping("/");
 
-        FilterRegistration.Dynamic registration = container.addFilter(
-                "loggingFilter", new LoggingFilter()
-        );
-        registration.addMappingForUrlPatterns(null, false, "/*");
+        ServletRegistration.Dynamic webDispatcher = container.addServlet("springDispatcher", new DispatcherServlet(servletContext));
+        
+        webDispatcher.setLoadOnStartup(1);  // Set load-on-startup only here
+        webDispatcher.setMultipartConfig(new MultipartConfigElement(
+        	    null, 20_971_520L, 41_943_040L, 512_000
+        	));
+        webDispatcher.addMapping("/");  // Only once
 
-        registration = container.addFilter(
-                "authenticationFilter", new AuthenticationFilter()
-        );
-        registration.addMappingForUrlPatterns(
-                null, false, "/registration", "/registration/*", "/chat", "/chat/*",
-                "/session", "/session/*"
-        );
+        // Logging filter
+        FilterRegistration.Dynamic loggingFilter = container.addFilter("loggingFilter", new LoggingFilter());
+        loggingFilter.addMappingForUrlPatterns(null, false, "/*");
+
+        // Authentication filter
+        FilterRegistration.Dynamic authenticationFilter = container.addFilter("authenticationFilter", new AuthenticationFilter());
+        authenticationFilter.addMappingForUrlPatterns(null, false, "/registration", "/registration/*", "/chat", "/chat/*", "/session", "/session/*");
     }
+
 }
