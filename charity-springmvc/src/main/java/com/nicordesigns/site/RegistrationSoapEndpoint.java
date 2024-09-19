546320186ce0e054c102a576cde374bdfeadeceb
NicorDesignsLLC <nicordesigns@gmail.com>
2024-09-19 06:40:54 -0400

    Closer to working SOAP Endpoints

--- charity-springmvc/src/main/java/com/nicordesigns/site/RegistrationSoapEndpoint.java
+++ charity-springmvc/src/main/java/com/nicordesigns/site/RegistrationSoapEndpoint.java
@@ -1,45 +1,39 @@
 package com.nicordesigns.site;
 
 import java.util.List;
-
 import javax.inject.Inject;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
-
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
-import org.springframework.ws.server.endpoint.annotation.Namespace;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
-import org.springframework.ws.server.endpoint.annotation.XPathParam;
 
 @Endpoint
-public class RegistrationSoapEndpoint
-{
-    private static final String NAMESPACE = "http://nicordesigns.com/xmlns/charityregistration";
+public class RegistrationSoapEndpoint {
+    private static final String NAMESPACE = "http://nicordesigns.com/xmlns/registration";  // Updated namespace
 
-    @Inject RegistrationService registrationService;
+    @Inject 
+    RegistrationService registrationService;
 
-    @PayloadRoot(namespace = "http://nicordesigns.com/xmlns/charityregistration", localPart = "getAllCharityRegistrationsRequest")
+    @PayloadRoot(namespace = NAMESPACE, localPart = "getAllRegistrationsRequest")
     @ResponsePayload
-    public RegistrationWebServiceList read() {
+    public RegistrationWebServiceList getAllRegistrations() {
         RegistrationWebServiceList list = new RegistrationWebServiceList();
         list.setValue(this.registrationService.getAllRegistrations());
         return list;
     }
 
-    @PayloadRoot(namespace = NAMESPACE, localPart = "CharityRegistrationRequest")
-    @Namespace(uri = NAMESPACE, prefix = "s")
+    @PayloadRoot(namespace = NAMESPACE, localPart = "RegistrationRequest")
     @ResponsePayload
-    public Registration read(@XPathParam("/s:charityRegistrationRequest/id") long id)
-    {
+    public Registration getRegistrationById(@RequestPayload Registration request) {
+        long id = request.getId();
         return this.registrationService.getRegistration(id);
     }
 
     @PayloadRoot(namespace = NAMESPACE, localPart = "createRegistration")
     @ResponsePayload
-    public Registration create(@RequestPayload RegistrationForm form)
-    {
+    public Registration createRegistration(@RequestPayload RegistrationForm form) {
         Registration charityRegistration = new Registration();
         charityRegistration.setUserName("WebServiceAnonymous");
         charityRegistration.setSubject(form.getSubject());
@@ -47,54 +41,48 @@ public class RegistrationSoapEndpoint
         charityRegistration.setFileAttachments(form.getAttachments());
 
         this.registrationService.save(charityRegistration);
-
         return charityRegistration;
     }
 
     @PayloadRoot(namespace = NAMESPACE, localPart = "deleteRegistration")
-    @Namespace(uri = NAMESPACE, prefix = "s")
-    public void delete(@XPathParam("/s:deleteCharityRegistration/id") long id) throws ResourceNotFoundException
-    {
-    	if(this.registrationService.getRegistration(id) == null)
+    @ResponsePayload
+    public void deleteRegistration(@RequestPayload Registration request) throws ResourceNotFoundException {
+        long id = request.getId();
+        if (this.registrationService.getRegistration(id) == null) {
             throw new ResourceNotFoundException();
+        }
         this.registrationService.deleteRegistration(id);
     }
 
+    // RegistrationForm class
     @XmlType(name = "SoapRegistrationForm", namespace = "http://www.nicordesigns.com/soap")
-    public static class RegistrationForm
-    {
+    public static class RegistrationForm {
         private String subject;
         private String body;
         private List<FileAttachment> fileAttachments;
 
-        public String getSubject()
-        {
+        public String getSubject() {
             return subject;
         }
 
-        public void setSubject(String subject)
-        {
+        public void setSubject(String subject) {
             this.subject = subject;
         }
 
-        public String getBody()
-        {
+        public String getBody() {
             return body;
         }
 
-        public void setBody(String body)
-        {
+        public void setBody(String body) {
             this.body = body;
         }
 
         @XmlElement(name = "attachment")
-        public List<FileAttachment> getAttachments()
-        {
+        public List<FileAttachment> getAttachments() {
             return fileAttachments;
         }
 
-        public void setAttachments(List<FileAttachment> attachments)
-        {
+        public void setAttachments(List<FileAttachment> attachments) {
             this.fileAttachments = attachments;
         }
     }
