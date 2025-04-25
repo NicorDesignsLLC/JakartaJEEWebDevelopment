d3a55c677defa7ec4d6b02c53289b87b194a13b0
Nicolaas Kock <nicocpk@gmail.com>
2025-04-25 06:28:18 -0400

    Moving over to MariaDB

---
+++ charity-springjpa/src/main/java/org/charity/springjpa/App.java
@@ -0,0 +1,13 @@
+package org.charity.springjpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
